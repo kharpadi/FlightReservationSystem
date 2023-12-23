c11d5ffbf46173fd460c5f029c23904e1f62ad11
kharpadi <nartcndn@gmail.com>
2023-12-24 00:02:25 +0300

    güncellendi

--- FlightReservationSystem/Program.cs
+++ FlightReservationSystem/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container...
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
 	builder.Configuration.GetConnectionString("DefaultConnection")
